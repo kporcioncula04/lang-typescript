19ee83219d50654312973e61184facee99295c7e
karolina porcioncula <59806218+kporcioncula04@users.noreply.github.com>
2025-01-09 11:54:45 -0500

    use type alias

--- src/index.ts
+++ src/index.ts
@@ -18,12 +18,34 @@ enum Size {
 }
 
 //objects
-let employee: {
+
+type Employee = {
   id: number;
   name: string;
   retire: (date: Date) => void;
-} = {
+};
+let employee: Employee = {
   id: 1,
   name: "Mosh",
   retire: (date: Date) => {},
 };
+
+let a = 100;
+let b = "Coffee";
+let c = [true, false, false];
+let d = { age: 20 };
+let e = [3];
+let f;
+let g = [];
+
+let song: {
+  title: string;
+  releaseYear: number;
+} = { title: "My song", releaseYear: 2025 }; //missing release year
+
+let prices = [100, 200, 300];
+prices[0] = 400; // string not a number
+
+function myFunc(a: number, b: number): number {
+  return a * b;
+} // does not return any value
