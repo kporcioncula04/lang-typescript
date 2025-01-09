5eca80103083f6b51a9eeaa9138eff251e8ace19
karolina porcioncula <59806218+kporcioncula04@users.noreply.github.com>
2025-01-09 15:48:07 -0500

    class instance

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,62 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
+function calculateTax(income) {
+    return income * 0.2;
+}
+let user = [1, "Mosh"];
+let sales = 123456789;
+let numbers = [1, 2, 3];
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let employee = {
+    id: 1,
+    name: "Mosh",
+    retire: (date) => { },
+};
+let a = 100;
+let b = "Coffee";
+let c = [true, false, false];
+let d = { age: 20 };
+let e = [3];
+let f;
+let g = [];
+let song = { title: "My song", releaseYear: 2025 };
+let prices = [100, 200, 300];
+prices[0] = 400;
+function myFunc(a, b) {
+    return a * b;
+}
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+let users = [
+    {
+        name: "John Smith",
+        age: 30,
+        occupation: "Software engineer",
+    },
+    {
+        name: "Kate Müller",
+        age: 28,
+    },
+];
+class Account {
+    constructor(id, owner, balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+    deposit(amount) {
+        if (amount <= 0)
+            throw new Error("invalid amount");
+        this.balance += amount;
+    }
+}
+let account = new Account(1, "kp", 0);
+account.deposit(100);
+console.log(account);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -49,3 +49,62 @@ prices[0] = 400; // string not a number
 function myFunc(a: number, b: number): number {
   return a * b;
 } // does not return any value
+
+type Customer = {
+  birthday?: Date;
+};
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(1);
+// console.log(customer?.birthday?.getFullYear());
+
+type Users = {
+  name: string;
+  age: number;
+  occupation?: string;
+};
+let users = [
+  {
+    name: "John Smith",
+    age: 30,
+    occupation: "Software engineer",
+  },
+  {
+    name: "Kate Müller",
+    age: 28,
+  },
+];
+
+// • Birds fly. Fish swim. A Pet can be a Bird or Fish. Use type aliases to represent these
+
+type Bird = {
+  fly: () => void;
+};
+type Fish = {
+  swim: () => void;
+};
+
+type Pet = Bird | Fish;
+
+class Account {
+  id: number;
+  owner: string;
+  balance: number;
+
+  constructor(id: number, owner: string, balance: number) {
+    this.id = id;
+    this.owner = owner;
+    this.balance = balance;
+  }
+
+  deposit(amount: number): void {
+    if (amount <= 0) throw new Error("invalid amount");
+    this.balance += amount;
+  }
+}
+
+let account = new Account(1, "kp", 0);
+account.deposit(100);
+console.log(account);
