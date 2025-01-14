4ff6286caa5661461b1d03451ccd21730f8468dd
karolina porcioncula <59806218+kporcioncula04@users.noreply.github.com>
2025-01-14 14:34:29 -0500

    decorators

--- dist/index.js
+++ dist/index.js
@@ -1,62 +1,20 @@
 "use strict";
-function calculateTax(income) {
-    return income * 0.2;
-}
-let user = [1, "Mosh"];
-let sales = 123456789;
-let numbers = [1, 2, 3];
-var Size;
-(function (Size) {
-    Size[Size["Small"] = 1] = "Small";
-    Size[Size["Medium"] = 2] = "Medium";
-    Size[Size["Large"] = 3] = "Large";
-})(Size || (Size = {}));
-let employee = {
-    id: 1,
-    name: "Mosh",
-    retire: (date) => { },
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-let a = 100;
-let b = "Coffee";
-let c = [true, false, false];
-let d = { age: 20 };
-let e = [3];
-let f;
-let g = [];
-let song = { title: "My song", releaseYear: 2025 };
-let prices = [100, 200, 300];
-prices[0] = 400;
-function myFunc(a, b) {
-    return a * b;
+function Component(constructor) {
+    console.log("Compoennt decorator called");
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDom = () => {
+        console.log("inserting the compoentn in the Dom");
+    };
 }
-function getCustomer(id) {
-    return id === 0 ? null : { birthday: new Date() };
-}
-let customer = getCustomer(1);
-let users = [
-    {
-        name: "John Smith",
-        age: 30,
-        occupation: "Software engineer",
-    },
-    {
-        name: "Kate Müller",
-        age: 28,
-    },
-];
-class Account {
-    constructor(id, owner, balance) {
-        this.id = id;
-        this.owner = owner;
-        this.balance = balance;
-    }
-    deposit(amount) {
-        if (amount <= 0)
-            throw new Error("invalid amount");
-        this.balance += amount;
-    }
-}
-let account = new Account(1, "kp", 0);
-account.deposit(100);
-console.log(account);
+let ProfileComponent = class ProfileComponent {
+};
+ProfileComponent = __decorate([
+    Component
+], ProfileComponent);
 //# sourceMappingURL=index.js.map
